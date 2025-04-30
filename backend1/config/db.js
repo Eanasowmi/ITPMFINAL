a6aa778eaa7e12cce683042ca8e2993fdda8b8b7
Eanasowmi <147256019+Eanasowmi@users.noreply.github.com>
2025-04-30 20:29:52 +0530

    main

---
+++ backend1/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // Timeout after 5 seconds instead of 10
+    });
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ MongoDB Connection Error: ${error.message}`);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
